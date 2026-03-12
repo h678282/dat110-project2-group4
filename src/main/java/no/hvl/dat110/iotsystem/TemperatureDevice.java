b3ba4ec6e76d7e898d32b944f73e6d6fa406e566
h678282 <sirnesingvild@gmail.com>
2026-03-12 11:39:56 +0100

    Completed Task C
    Implemented classes: DisplayDevice and TemperatureDevice

--- src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
+++ src/main/java/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -12,19 +12,29 @@ public class TemperatureDevice {
 		// simulated / virtual temperature sensor
 		TemperatureSensor sn = new TemperatureSensor();
 
-		// TODO - start
-
 		// create a client object and use it to
+		Client client = new Client("sensor", Common.BROKERHOST, Common.BROKERPORT);
 
 		// - connect to the broker - user "sensor" as the user name
-		// - publish the temperature(s)
-		// - disconnect from the broker
+		client.connect();
 
-		// TODO - end
+		// - publish the temperature(s)
+		for (int i = 0; i < COUNT; i++) {
+			int temp = sn.read();
+			System.out.println("READING: " + temp);
+
+			client.publish(Common.TEMPTOPIC, Integer.toString(temp));
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// - disconnect from the broker
+		client.disconnect();
 
 		System.out.println("Temperature device stopping ... ");
-
-		throw new UnsupportedOperationException(TODO.method());
-
 	}
 }
